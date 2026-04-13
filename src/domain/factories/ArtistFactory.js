7a7a2fef700f65053936bcc629b2e7bbce064f82
lestaplus <shevtsov190107@gmail.com>
2026-04-13 19:06:37 +0300

    create artist factory

---
+++ src/domain/factories/ArtistFactory.js
@@ -0,0 +1,23 @@
+import { Artist } from "../models/Artist.js";
+import { DomainError } from "../errors/DomainError.js";
+
+export class ArtistFactory {
+  constructor(artistRepository) {
+    this.artistRepository = artistRepository;
+  }
+
+  async create({ name, genre, foundedYear }) {
+    const existingArtist = await this.artistRepository.findByName(name);
+
+    if (existingArtist) {
+      throw new DomainError(`Artist with name ${name} already exists`);
+    }
+
+    return new Artist({
+      id: null,
+      name,
+      genre,
+      foundedYear,
+    });
+  }
+}
